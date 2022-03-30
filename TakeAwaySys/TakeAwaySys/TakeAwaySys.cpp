ad1066f84e617dd01cea73287239429f85ef281c
AJ <aletha.e.jones@gmail.com>
2022-03-30 17:06:35 +0100

    dev-sync

--- TakeAwaySys/TakeAwaySys/TakeAwaySys.cpp
+++ TakeAwaySys/TakeAwaySys/TakeAwaySys.cpp
@@ -8,15 +8,206 @@
 #include "FoodItem.h"
 #include "Graphics.h"
 #include "ScrClnTM.h"
-#include "CommaRemover.h"
 
 using namespace std;
 
-CommaRemover CRHandler;
 ScrClnTM SCHandler;
 Graphics GHandler;
 const string CurrencySym = "\x9C";
 string MenuTitle;
+
+class CommaRemover
+{
+public:
+	CommaRemover()
+	{
+		Temp = "";
+		Result = "";
+	};
+
+	string Remove(char Removable, string Input, string Option) //[left/right]
+	{
+
+
+		for (int i = 0; i < Input.length(); i++)
+		{
+			if (Input[i] != Removable)
+			{
+				Temp += Input[i];
+			}
+			else if (Input[i] == Removable)
+			{
+				Temps.push_back(Temp);
+				Temp.clear();
+			}
+			Temps.push_back(Temp);
+
+			for (int j = 0; j < Temps.size(); j++)
+			{
+				if (Option == "left")		//removes first section
+				{
+					if (j != 0)
+					{
+						Result += Temps[j];
+					}
+				}
+				else if (Option == "right")		//removes last section
+				{
+					if (j != Temps.size())
+					{
+						Result += Temps[j];
+					}
+				}
+
+			}
+		}
+		return Result;
+	}
+
+private:
+	vector <string> Temps; string Temp, Result;
+};
+
+class Date
+{
+public:
+	Date();
+
+	void SetTime()
+	{
+		do
+		{
+			cout << "Please enter the current: " << endl;
+			cout << "Hour (24hr): ";
+			cin >> Input;
+
+			while (stoi(Input) > 24 || stoi(Input) < 0)
+			{
+				cout << "Please enter a value between [0] and [24]: ";
+				cin >> Input;
+			}
+			Hour = stoi(Input);
+			Input.clear();
+
+			cout << "Minute: ";
+			cin >> Input;
+
+			while (stoi(Input) > 60 || stoi(Input) < 0)
+			{
+				cout << "Please enter a value between [0] and [60]: ";
+				cin >> Input;
+			}
+			Minute = stoi(Input);
+			Input.clear();
+
+			cout << "Confirm time? [yes/no]" << endl << "> ";
+			cin >> Input;
+		} while (Input == "no");
+	}
+	
+	void SetDate()
+	{
+		do
+		{
+			cout << "Please only enter numerical values" << endl;
+			cout << "Please enter the:" << endl;
+			cout << "Year: ";
+			cin >> Year;
+
+			cout << "Month: ";
+			cin >> Input;
+
+			while (stoi(Input) > 12 || stoi(Input) < 1)
+			{
+				cout << "Please enter a value between [1] and [12]: ";
+				cin >> Input;
+			}
+			Month = stoi(Input);
+			Input.clear();
+
+			cout << "Day: ";
+			cin >> Input;
+
+			do		//working here pal
+			{
+				if (stoi(Input) != MonthDays[Month-1])
+				{
+					cout << "Month " << Month << " only has " << MonthDays[Month-1];
+					cout << "Please enter a value between [1] and [" << MonthDays[Month-1] << "]";
+				}
+
+			} while (true);
+
+			cout << "Confirm Date? [yes/no]" << endl << "> ";
+			cin >> Input;
+		} while (Input == "no");
+	}
+
+	void SetDateTime()
+	{
+		SetTime();
+		SetDate();
+	}
+
+	bool operator>(Date D2)
+	{
+		if (this->Year < D2.Year)
+		{return false;}
+		else if (this->Year > D2.Year)
+		{return true;}
+		else if (this->Month > D2.Month)
+		{return true;}
+		else if (this->Day > D2.Day)
+		{return true;}
+		else if (this->Hour > D2.Hour)
+		{return true;}
+		else if (this->Minute > D2.Minute)
+		{return true;}
+		else 
+		{return false;}
+	}
+
+	bool operator<(Date D2)
+	{
+		if (this->Year > D2.Year)
+		{return false;}
+		else if (this->Year < D2.Year)
+		{return true;}
+		else if (this->Month < D2.Month)
+		{return true;}
+		else if (this->Day < D2.Day)
+		{return true;}
+		else if (this->Hour < D2.Day)
+		{return true;}
+		else if (this->Minute < D2.Minute)
+		{return true;}
+		else
+		{return false;}
+	}
+
+	string GetDate()
+	{
+		Temp = Hour + ":" + Minute;
+		Temp += " " + Day + '/' + Month + '/' + Year;
+		return Temp;
+	}
+
+	void SplitDate(string Input)
+	{
+
+	}
+
+private:
+	int Day;
+	int Month;
+	int Year;
+	int Minute;
+	int Hour;
+
+	int MonthDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+	string Input, Temp;
+};
 
 class Customer
 {
@@ -116,7 +307,7 @@ public:
 
 		MenuTitle += "New Cuisine \n";
 
-		double Price;
+		//double Price;
 
 		Input.clear();
 		
@@ -205,7 +396,7 @@ public:
 			{
 				cout << Cuisine[i] << endl;
 				for (int j = 0; j < Dishes.size(); j++)
-				{					
+				{
 					if (Dishes[j].GetCuisine() == CRHandler.Remove(',',Cuisine[i],"left"))
 					{
 						Dishes[j].DisplayDish();
@@ -260,9 +451,9 @@ public:
 
 private:
 	vector <FoodItem> Dishes;
-
 	vector <string> Cuisine;
-	//work out nicer way to handle  cuisine name and prefix
+
+	CommaRemover CRHandler;
 };
 
 class Order
@@ -281,10 +472,11 @@ public:
 	}
 
 private:
-	string OrderID;
-	bool Open;
 	vector <FoodItem> Basket;
 	Customer Cust;
+	Date OrderDate;
+	string OrderID;
+	bool Open;
 	bool Delivery;
 	double BasketPrice;
 	double Total;
